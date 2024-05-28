d37099a32cd329cd3998b01d21d167bab6d9f07d
Johnmabry3 <johnmabry3@gmail.com>
2024-05-28 14:13:52 -0400

    Initial Commit

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -3,5 +3,50 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(200);
+
+  
+ 
+
+  fill('#DDC077');
+  stroke('#523A28');
+  strokeWeight(3);
+  rect(165, 200, 65, 175, 85);
+
+    stroke('#523A28')
+    strokeWeight(2);
+    line(195, 300, 175, 365); 
+
+    stroke('#523A28')
+    strokeWeight(2);
+    line(215, 300, 195, 375); 
+
+
+      fill('#5CD85A');
+      stroke('#013A20');
+      strokeWeight(3);
+      rect(108, 55, 175, 250, 95);
+
+
+          fill('#F2E437');
+          stroke('#013A20');
+          strokeWeight(3);
+          ellipse(195, 175, 175, 200);
+
+
+              fill('#DC7726');
+              stroke('#013A20');
+              strokeWeight(3);
+              ellipse(195, 150, 175, 195);
+
+
+  fill('#9D0F40');
+  stroke('#013A20');
+  strokeWeight(4);
+  ellipse(195, 130, 175, 175);
+
+
+ 
+
+
 }
