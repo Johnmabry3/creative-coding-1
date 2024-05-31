ba02d114834432ff373b7163d20ceec0426fac21
Johnmabry3 <johnmabry3@gmail.com>
2024-05-31 11:31:27 -0400

    Initial Commit

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,13 +1,38 @@
+
+let img;
+let sampleX = 20;
+let sampleY = 10;
+
 function preload(){
-	img = loadImage();
+	img = loadImage('https://johnmabry3.github.io/creative-coding-1/week2/pixels/IMG_3074.jpg');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600,745);
+
+  img.loadPixels()
   
 }
 
 function draw() {
   background(220);
-image(img,10,10,100,100);
+
+  noStroke();
+
+  translate(0,0);
+  image(img,0,0,600,745);
+
+  for (let x = 0; x <= img.width; x += sampleX){
+    for (let y = 0; y <= img.height; y += sampleY){
+      var index = (x + y * img.width) * 2
+      var r = img.pixels[index + 0];
+      var g = img.pixels[index + 1];
+      var b = img.pixels[index + 2];
+
+      //fill(r,g,b);
+      ellipse(x,y,sampleX,sampleY);
+    }
+  }
+
+
 }
