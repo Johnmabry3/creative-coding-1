6a6683c9cb0e67bc5689b562245eeb234330dd5e
Johnmabry3 <johnmabry3@gmail.com>
2024-06-03 11:56:06 -0400

    Initial Commit

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,7 +1,34 @@
+let x = 0;
+let y = 0;
+let xspeed = 4;
+let yspeed = 4;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  fill("green");
 }
 
 function draw() {
   background(220);
+  
+
+  translate(width / 2, height / 2);
+
+  rect(x,y,55);
+
+
+  if (x > 145 | x < -200){
+    xspeed = - xspeed;
+    fill(random(255),random(255),random(255));
+  }
+
+  if (y > 95 | y < -150){
+    yspeed = - yspeed;
+    fill(random(255),random(255),random(255));
+  }
+
+  x += xspeed;
+  y += yspeed;
+  
 }
