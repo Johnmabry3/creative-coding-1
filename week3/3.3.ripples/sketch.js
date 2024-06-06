c25fe74da620e540245ae912b083315aa3108767
Johnmabry3 <johnmabry3@gmail.com>
2024-06-05 21:31:41 -0400

    Initial Commit

--- week3/3.3.ripples/sketch.js
+++ week3/3.3.ripples/sketch.js
@@ -1,7 +1,56 @@
+let ripple_x = 0;
+let ripple_y = 0;
+let ripple_d = 0;
+
+let ripplers = [];
+
+class Rippler {
+  constructor (x,y){
+    this.x = x,
+    this.y = y,
+    this.d = 0
+  }
+
+  draw(){
+    this.d += 3;
+    circle(this.x, this.y, this.d);
+  }
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas( 800, 800);
 }
 
 function draw() {
-  background(220);
+  background('#53c7f0');
+  stroke("white");
+  strokeWeight(5);
+  noFill();
+
+  for (let i = 0; i < ripplers.length; i++){
+    ripplers[i].draw();
+  }
+
+  //ripple_d += 1;
+  //circle(ripple_x, ripple_y, ripple_d);
+
+  //push();
+  //fill("white");
+  //strokeWeight(1);
+  //textSize(30);
+  //text("ripple_x:" + ripple_x, 50, 50);
+  //text("ripple_y:" + ripple_y, 50, 80);
+  //pop();
+}
+
+function mousePressed(){
+
+  ripple_x = mouseX;
+  ripple_y = mouseY;
+  ripple_d = 0;
+
+  
+
+  ripplers.push(new Rippler(mouseX, mouseY));
+
 }
