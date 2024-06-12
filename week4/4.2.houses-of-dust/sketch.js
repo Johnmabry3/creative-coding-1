3a00cd00e5ac21c70fe3aa82c3fc50682388bad8
Johnmabry3 <johnmabry3@gmail.com>
2024-06-12 10:52:13 -0400

    Recent Commit

--- week4/4.2.houses-of-dust/sketch.js
+++ week4/4.2.houses-of-dust/sketch.js
@@ -1,27 +1,24 @@
 
 let grammar = tracery.createGrammar({
-"origin":["A HOUSE OF #material#"],
-"material":["STICKS","PLASTIC","RUBBER","FOAM","PAPER",
-            "PAINT","OIL","WATER","INK","GLASS","GRASS",
-            "LINEN","WOOD","TAPE","METHANE","WAX","SILICONE"]
-//"placement":["IN A CABIN","IN THE MIDDLE OF NOWHERE","ON A MOUNTAIN"]
-//"conducting":[]
-//"whom":[]
+"origin":["A HOUSE OF #material#\n#place#\nUSING #light_source#\nINHABITED BY #inhabitants#"],
+"material":["STICKS","PLASTIC","RUBBER","FOAM","PAPER","PAINT","OIL","WATER","INK","GLASS","GRASS","LINEN","WOOD","TAPE","METHANE","WAX","SILICONE"],
+"place":["IN A CABIN","IN THE MIDDLE OF NOWHERE","ON A MOUNTAIN","IN A WINTER PINE FOREST","ON A BEACHFRONT","ON THE HORIZON","ON CAPITOL HILL","ON A DESERTED ISLAND","IN A FACTORY","ON A FARM","ON THE MOON","IN WASHINGTON D.C."],
+"light_source":["EVERY PIECE OF COAL","SUNLIGHT","CANDLELIGHT","LED LIGHTING","ELECTRICTY","GASLIGHTING"],
+"inhabitants":["STRANGERS","ENEMIES","MAGICIANS AND SORCERERS","OIL COMPANIES","ALL OF MANKIND","ASTRONAUTS","ARCHITECTS","GREEDY PEOPLE"],
 
 });
 
-let poem = grammar.flatten('#origin#');
+//let poem = grammar.flatten('#origin#');
 
 function setup() {
   createCanvas(800, 800);
-  frameRate(0.40);
-  noLoop();
+  frameRate(0.15);
 }
 
 function draw() {
   background(220);
   poem = grammar.flatten("#origin#");
   textFont("courier new");
-  textSize(40);
+  textSize(32);
   text(poem,50,150);
 }
