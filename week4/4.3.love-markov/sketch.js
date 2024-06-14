dba01a6edc96a78955d98308bb13430f0dcc3aa8
Johnmabry3 <johnmabry3@gmail.com>
2024-06-13 23:12:22 -0400

    Initial Commit

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,14 +1,45 @@
 
+// create markov model
+let rm = RiTa.markov(4);
 
-let rm = RiTa.markov(2);
+// find data
+let data = document.getElementById("source").innerText;
+
+// training the model
+rm.addText(data);
 
 
 
-function preload() {
-  result = loadStrings('');
-}
+//function preload() {
+  //result = loadStrings('');
+//}
 
 function setup(){
 
-	
+	// generate # of sentences
+	let lines = rm.generate(10);
+
+	// finding the corresponding letter DIV
+	let letter = select("#letter");
+
+	// add greeting
+	letter.child( createP("Dear Sallie,"));
+
+	for (let l = 0; l < lines.length; l++){
+		letter.child(
+			createP( lines[l] )
+			);
+	}
+
+	// add a complimentary closing
+	letter.child( createP("Yours Truly, <br>Johnny"));
+
+
+}
+
+function mousePressed(){
+	letter.remove();
+	letter = createDiv();
+	letter.id("letter");
+	setup();
 }
