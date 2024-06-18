d1a47adb3ceacdf0dd3607938615eab0ecde0108
Johnmabry3 <johnmabry3@gmail.com>
2024-06-18 10:04:41 -0400

    Updated Commit

---
+++ week2/2.1.emoji/sketch.v2.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400,550);
+}
+
+function draw() {
+  background("#B99095");
+
+  fill("#F7E2C7");
+  rect(165,300,65,175,85);
+ 
+
+  fill("#F1C0B9");
+  ellipse(195,270,175,175);
+
+    fill("#1D741B");
+    ellipse(195,240,175,175);
+
+      fill("#D2FBA4");
+      ellipse(195,210,175,175);
+
+        fill("#F1C0B9");
+        ellipse(195,180,175,175);
+
+          fill("#88CA5E");
+          ellipse(195,150,175,175);
+          noStroke();
+      
+}
